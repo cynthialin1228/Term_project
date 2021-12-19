c6d3fd393c205c0072ff89af29b0db3eb7768ce3
cynthialin1228 <70316322+cynthialin1228@users.noreply.github.com>
2021-12-19 17:45:31 +0800

    Add files via upload
    Main script with some SDL. However, it's still extremely raw, with only function names that indicates what step it is.

---
+++ finalProject_20211219/cmake-build-debug/CMakeFiles/finalProject_20211219.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/finalProject_20211219.dir/main.cpp.o"
+  "finalProject_20211219"
+  "finalProject_20211219.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/finalProject_20211219.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ finalProject_20211219/cmake-build-debug/CMakeFiles/finalProject_20211219.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for finalProject_20211219.
+# This may be replaced when dependencies are built.
